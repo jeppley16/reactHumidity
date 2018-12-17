61544ce47aebc496ac8ad009c70513b58ed5d5af
jeppley16 <justin.eppley@gmail.com>
2018-12-17 15:58:44 -0500

    Adding cities object for testing

--- reacthumidity/src/Humidity.js
+++ reacthumidity/src/Humidity.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import './Humidity.css';
 import 'tachyons';
 
-const Humidity = () => {
+const Humidity = ({humidity}) => {
 	return (
 		<div className='humidifier'>
 			<div className='tank'>
 				<div className='waterLevel'></div>
 			</div>
 				<div className='operations'>
-					<div className='stats tc'>
-						<h2>Humidity:</h2>
-						<p>56%</p>
+					<div className='dial tc'>
+						<div className='dial-inner'></div>
+						<div className='indicator'></div>
+						<div className='stat'>
+							<p>{humidity * 100 + '%'}</p>
+						</div>
 					</div>
 				</div>
 				<div className='base'></div>
---
+++ reacthumidity/src/cities.js
@@ -0,0 +1,28 @@
+export const cities = [
+	{
+		id: 1,
+		city: 'Swampscott',
+		humidity: 0.45
+
+	},
+	{
+		id: 2,
+		city: 'Marblehead',
+		humidity: 0.55
+		
+	},
+	{
+		id: 3,
+		city: 'Boston',
+		humidity: 1.0
+		
+	},
+	{
+		id: 4,
+		city: 'Woburn',
+		humidity: 0.25
+		
+	}
+
+
+]
